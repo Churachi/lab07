a3ecc9f912efb5a8faa443f226cd3b601ab3aa53
Churachi <cmtam@usc.edu>
2023-10-19 23:54:07 -0700

    Configurations Added

--- TestSuite.py
+++ TestSuite.py
@@ -1,7 +1,18 @@
 import RPi.GPIO as GPIO
 import time
+import Adafruit_GPIO.SPI as SPI
+import Adafruit.MCP3008
+
+# using physical pin 11 to blink an LED
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+chan_list = [11]
+GPIO.setup(chan_list, GPIO.OUT)
+
+# Hardware SPI Configuration
+SPI_PORT = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
 
 while True:
     #Blink the LED 5 times with on/off intervals of 500ms
